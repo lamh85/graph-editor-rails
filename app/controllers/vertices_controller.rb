c7af2a7bd73103da33f135ea26f272d4daa8657f
lamh85 <lamh85@gmail.com>
2024-02-22 16:16:28 -0800

    controller action vertex#patch

--- app/controllers/vertices_controller.rb
+++ app/controllers/vertices_controller.rb
@@ -3,4 +3,23 @@ class VerticesController < ApplicationController
     vertices = Vertex.all
     render json: vertices
   end
+
+  def patch
+    vertex = Vertex.find(params[:id])
+    
+    begin
+      vertex.update!(vertex_params)
+    rescue => error
+      render json: { message: "Error type: #{error.class.to_s}" }
+    end
+
+    render json: vertex.reload
+  end
+
+  private
+
+    def vertex_params
+      params.require(:vertex).permit(:centre_x, :centre_y, :height, :width, :radius, :shape)
+    end
+
 end
